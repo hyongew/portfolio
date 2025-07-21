bd7027373a2a4ac1c7da8780c2a673110e923e3f
hyongew <hongyew98@gmail.com>
2025-07-21 23:08:11 +0800

    [feat] slight tweak to resume page styling

--- src/pages/resume/resume.tsx
+++ src/pages/resume/resume.tsx
@@ -4,11 +4,13 @@ export default function Resume() {
     <>
       <section className="mb-3" style={{textAlign: "justify"}}>
         <h6 className="resume-intro">Software Engineer <span className="fade-in">at DBS Bank.<a href="/portfolio/blank.pdf" target="_blank" aria-label="Resume" rel="noopener noreferrer" style={{textDecoration: "unset"}} className="underline-btn ms-4">Resume</a></span></h6>
-        <p className="h5 fst-italic">I like building clean and straightforward user experiences. <span className="h6 fst-normal fade-in__right" style={{animationDelay: "0.1s"}}>That extends to building systems that are not just functional, but also maintainable and scalable. To me, it starts with writing clean, readable code, supported by good test coverage.</span></p>
+        <p className="h5 fst-italic" style={{textAlign: "start"}}>I like building clean and straightforward user experiences.</p>
         <br />
-        <p className="h6 fade-in__right" style={{animationDelay: "0.2s"}}>One of my favourite things about programming is seeing the different components of a well-structured system come together. I also enjoy solving problems through logical thinking. Over time, however, I've learned that <strong>the best solutions don't just depend on technical elegance</strong>. They also need to consider real-world constraints like time, maintainability, and how easily it can be communicated to others.</p>
+        <p className="h6 fade-in__down" style={{animationDelay: "0.1s"}}>That extends to building systems that are not just functional, but also maintainable and scalable. To me, it starts with writing clean, readable code, supported by good test coverage.</p>
         <br />
-        <p className="h6 fade-in__right" style={{animationDelay: "0.3s"}}>I believe that good teams have a <strong>clear purpose</strong> and <strong>clearly defined roles</strong> so that everyone is able to make their highest level of contribution. Feel free to reach out if you are building a team with this in mind!</p>
+        <p className="h6 fade-in__down" style={{animationDelay: "0.2s"}}>One of my favourite things about programming is seeing the different components of a well-structured system come together. I also enjoy solving problems through logical thinking. Over time, however, I've learned that <strong>the best solutions don't just depend on technical elegance</strong>. They also need to consider real-world constraints like time, maintainability, and how easily it can be communicated to others.</p>
+        <br />
+        <p className="h6 fade-in__down" style={{animationDelay: "0.3s"}}>I believe that good teams have a <strong>clear purpose</strong> and <strong>clearly defined roles</strong> so that everyone is able to make their highest level of contribution. Feel free to reach out if you are building a team with this in mind!</p>
       </section>
     </>
   )
